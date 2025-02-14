00f29add8f99e63a4b8b991d43b1945118bdc620
TheAIHumansDev <161964439+TheAIHumansDev@users.noreply.github.com>
2025-02-14 11:20:07 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 <script
    src="https://bit.ly/grupochess-server"
-   data-sm-api-key="eyJzb3VsSWQiOiJkZG5hLWNsaW5pY2EtamVuaXJlZS1vcmcxZWVhLS1leHBsb3JlciIsImF1dGhTZXJ2ZXIiOiJodHRwczovL2RoLnNvdWxtYWNoaW5lcy5jbG91ZC9hcGkvand0IiwiYXV0aFRva2VuIjoiYXBpa2V5X3YxXzY0MGJjODNjLTkxM2EtNGNjZi05NWQ4LTIzMTNkYjM4NjViNyJ9"
+   data-sm-api-key="eyJzb3VsSWQiOiJkZG5hLWRlbW8tc3BhY2Utb3JnY2JlYy0tc2lldGVkaWFzIiwiYXV0aFNlcnZlciI6Imh0dHBzOi8vZGguc291bG1hY2hpbmVzLmNsb3VkL2FwaS9qd3QiLCJhdXRoVG9rZW4iOiJhcGlrZXlfdjFfY2I1ZjA3MjEtNThiOC00OGU0LTk2YTAtZmIzMDU5OGZhMTVhIn0="
    data-sm-position="bottomLeft"
    data-sm-greeting="Saluda a UNEDIA"
    data-sm-layout="fullFrame"  

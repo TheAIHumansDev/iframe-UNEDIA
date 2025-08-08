22d5d354454fa88f7d28cd6f9f995315dbf9e3b5
TheAIHumansDev <161964439+TheAIHumansDev@users.noreply.github.com>
2025-08-08 12:08:36 -0600

    Update CONED.js

--- CONED.js
+++ CONED.js
@@ -1,6 +1,6 @@
 <script
    src="https://bit.ly/grupochess-server"
-   data-sm-api-key="eyJzb3VsSWQiOiJkZG5hLXNpZXRlLWFnb3N0by1vcmctLWV4cGxvcmVybmVkIiwiYXV0aFNlcnZlciI6Imh0dHBzOi8vZGguc291bG1hY2hpbmVzLmNsb3VkL2FwaS9qd3QiLCJhdXRoVG9rZW4iOiJhcGlrZXlfdjFfNTcyNTNkMWYtY2MwMi00MjQ0LTkzM2MtNmE1MGViNTRiOTM3In0="
+   data-sm-api-key="eyJzb3VsSWQiOiJkZG5hLXNpZXRlLWFnb3N0by1vcmctLWV4cGxvcmVyY29uZWQiLCJhdXRoU2VydmVyIjoiaHR0cHM6Ly9kaC5zb3VsbWFjaGluZXMuY2xvdWQvYXBpL2p3dCIsImF1dGhUb2tlbiI6ImFwaWtleV92MV9iNTI3Y2Q4ZC0wNDU2LTRiNTgtOTk2ZC05NGNjMGNhMWU2YjUifQ=="
    data-sm-position="bottomLeft"
    data-sm-greeting="Saluda a CONI"
    data-sm-layout="fullFrame"  

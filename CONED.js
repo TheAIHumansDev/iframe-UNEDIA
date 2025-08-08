84b28006903f2ce456ba444b37f67ce7fe64f75e
TheAIHumansDev <161964439+TheAIHumansDev@users.noreply.github.com>
2025-08-08 12:05:45 -0600

    Create CONED.js

---
+++ CONED.js
@@ -0,0 +1,8 @@
+<script
+   src="https://bit.ly/grupochess-server"
+   data-sm-api-key="eyJzb3VsSWQiOiJkZG5hLXNpZXRlLWFnb3N0by1vcmctLWV4cGxvcmVybmVkIiwiYXV0aFNlcnZlciI6Imh0dHBzOi8vZGguc291bG1hY2hpbmVzLmNsb3VkL2FwaS9qd3QiLCJhdXRoVG9rZW4iOiJhcGlrZXlfdjFfNTcyNTNkMWYtY2MwMi00MjQ0LTkzM2MtNmE1MGViNTRiOTM3In0="
+   data-sm-position="bottomLeft"
+   data-sm-greeting="Saluda a CONI"
+   data-sm-layout="fullFrame"  
+   data-sm-profile-picture="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExaXkzZmZ2b25tczVqMnUwNTNmb3N1eHE2bnR4eTk5dWhjdDZpdHV0cSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/iunm6mcbCzSgWGzcbo/giphy.gif" 
+ ></script>
